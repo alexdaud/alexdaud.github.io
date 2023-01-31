cc5f66badab43e460f56b27cda014f38eb485439
Alex Daud <alexander.daud@ozan.com>
2023-01-31 20:55:12 +0000

    added react router dom and deep linked tabs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { QUOTE_GENERATOR } from "./projects/Projects";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to={"/" + QUOTE_GENERATOR} />,
+  },
+  {
+    path: ":project",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,18 +1,52 @@
 import React from "react";
 import ToDo from "./ToDo/ToDo";
 import QuoteGenerator from "./QuoteGenerator/QuoteGenerator";
+import { useNavigate, useParams } from "react-router-dom";
+import "./Projects.css";
 
-class Projects extends React.Component {
-  render() {
-    return (
-      <div className="flex flex-col items-center justify-center">
-        <div className="py-0 px-5 w-full max-w-xl">
-          <ToDo />
-          <hr className="h-px my-7 bg-slate-700 border-0 dark:bg-slate-400" />
-          <QuoteGenerator />
+export const QUOTE_GENERATOR = "quote-generator";
+export const TO_DO = "to-do";
+
+function Projects() {
+  const navigate = useNavigate();
+
+  const { project } = useParams();
+
+  const handleTabClick = (project) => {
+    navigate(`/${project}`);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="py-0 px-5 w-full max-w-xl">
+        <div className="text-sm font-medium text-center text-slate-800 border-b text-slate-500 self-start">
+          <ul className="flex flex-wrap -mb-px">
+            <li className="mr-2">
+              <button
+                onClick={() => handleTabClick(QUOTE_GENERATOR)}
+                className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-slate-600 hover:border-slate-300 ${
+                  project === QUOTE_GENERATOR ? "active" : ""
+                }`}
+              >
+                Quote Generator
+              </button>
+            </li>
+            <li className="mr-2">
+              <button
+                onClick={() => handleTabClick(TO_DO)}
+                className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-slate-600 hover:border-slate-300 ${
+                  project === TO_DO ? "active" : ""
+                }`}
+              >
+                To Do
+              </button>
+            </li>
+          </ul>
         </div>
+        {project === QUOTE_GENERATOR && <QuoteGenerator />}
+        {project === TO_DO && <ToDo />}
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default Projects;
